c4625ed11ab144e8fe31cb23e0e20c2b46cf4d6a
Renato Khael <renatokhael@gmail.com>
2025-10-19 22:53:13 -0300

    bug: adjust malito loop

--- src/app/components/documentation/Link.tsx
+++ src/app/components/documentation/Link.tsx
@@ -9,10 +9,14 @@ export const Link = ({
   children: React.ReactNode;
   className?: string;
 }) => {
+  // Não usar target="_blank" para links mailto
+  const isMailto = href.startsWith("mailto:");
+
   return (
     <a
       href={href}
-      target="_blank"
+      target={isMailto ? undefined : "_blank"}
+      rel={isMailto ? undefined : "noopener noreferrer"}
       className={cx(
         "underline underline-offset-2 hover:decoration-2",
         className
